f8fbd6d95339f4e497e51ce10dc2bb0d93d5d521
Joachim Ziebs <joachim@ziebs.net>
2017-10-30 23:02:40 +0100

    Added a way to choose the number of LEDs.

--- src/Christmas_Wraith.ino
+++ src/Christmas_Wraith.ino
@@ -11,14 +11,34 @@ char b = 'b';
 rgbLED candle[4]={rgbLED(2, 3, 4), rgbLED(5, 6, 7), rgbLED(8, 9, 10), rgbLED(11, 12, 13)};
 
 void setup() {
-	
 	SoftPWMBegin(); //necessary because it is used in rgbLED.h
 	randomSeed(analogRead(0)); // some randomness to get candle flickers later
+	pinMode(A1, INPUT); // these are used to set the number of candles
+	pinMode(A2, INPUT); // use a cable between GND and A1-A4
+	pinMode(A3, INPUT);
+	pinMode(A4, INPUT);
+	digitalWrite(A1, INPUT_PULLUP);
+	digitalWrite(A2, INPUT_PULLUP);
+	digitalWrite(A3, INPUT_PULLUP);
+	digitalWrite(A4, INPUT_PULLUP);
 }
 
 void loop() {
 
-	for(unsigned int i = 0; i < 4; i++) {
+	unsigned int maxCandles = 0;
+
+	Serial.println(digitalRead(A1));
+	if (digitalRead(A1) == LOW) {
+		maxCandles = 1;
+	} else if (digitalRead(A2) == LOW) {
+		maxCandles = 2;
+	} else if (digitalRead(A3) == LOW) {
+		maxCandles = 3;
+	} else if (digitalRead(A4) == LOW) {
+		maxCandles = 4;
+	}
+
+	for(unsigned int i = 0; i < maxCandles; i++) {
 		candle[i].on(r, random(10, 50)+30);
 		candle[i].on(g, random(10, 25));
 		candle[i].on(b, random(10));
@@ -39,6 +59,8 @@ void loop() {
 	// candle[i].singleoff(b);
 	// delay(500);
 		}
+
 	delay(350);
+
 }
 
